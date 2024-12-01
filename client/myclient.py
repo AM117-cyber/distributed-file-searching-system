4c7c0ae233175c46663b9386bb581e1e745f0d8b
Melissa <anamelissa1707@gmail.com>
2024-12-01 02:05:36 -0800

    updating server and client to work with sqlite db

--- client/myclient.py
+++ client/myclient.py
@@ -5,37 +5,41 @@ def upload_file(client_socket, filename):
     if os.path.isfile(filename):
         filesize = os.path.getsize(filename)
         client_socket.send(f'UPLOAD == {filename} == {filesize}'.encode())
-        with open(filename, 'rb') as f:
-            data = f.read(1024)
-            while data:
-                client_socket.send(data)
-                data = f.read(1024)
-        response = client_socket.recv(1024).decode()
-        print(response)
+        confirm = client_socket.recv(1048576).decode()
+        print(confirm)
+        if confirm.startswith('Valid'):
+            with open(filename, 'rb') as f:
+                data = f.read(1048576)
+                while data:
+                    client_socket.send(data)
+                    data = f.read(1048576)
+        
+            response = client_socket.recv(1048576).decode()
+            print(response)
     else:
         print("File not found")
 
-def search_file(client_socket, query, file_type):
-    client_socket.send(f'SEARCH {query} {file_type}'.encode())
-    response = client_socket.recv(1024).decode()
+def search_file(client_socket, file_name, file_type):
+    client_socket.send(f'SEARCH == {file_name} == {file_type}'.encode())
+    response = client_socket.recv(1048576).decode()
     if response.startswith('FOUND'):
         num_files = int(response.split()[1])
         for _ in range(num_files):
-            filename = client_socket.recv(1024).decode()
+            filename = client_socket.recv(1048576).decode()
             print(f"Found: {filename}")
             client_socket.send(b'ACK')
     else:
         print("No files found")
 
 def download_file(client_socket, filename):
-    client_socket.send(f'DOWNLOAD {filename}'.encode())
-    response = client_socket.recv(1024).decode()
-    if response.startswith('EXISTS'):
+    client_socket.send(f'DOWNLOAD == {filename}'.encode())
+    response = client_socket.recv(1048576).decode()
+    if response.startswith('FileSize'):
         filesize = int(response.split()[1])
         with open('downloaded_' + filename, 'wb') as f:
             bytes_received = 0
             while bytes_received < filesize:
-                data = client_socket.recv(1024)
+                data = client_socket.recv(1048576)
                 f.write(data)
                 bytes_received += len(data)
         print("Download complete.")
@@ -55,7 +59,7 @@ def main():
         elif command.startswith('SEARCH'):
             filename = input("Enter file name: ")
             file_type = input("Enter file type: ")
-            search_file(client_socket, query, file_type)
+            search_file(client_socket, filename, file_type)
         elif command.startswith('DOWNLOAD'):
             filename = input("Enter command file name: ")
             download_file(client_socket, filename)
