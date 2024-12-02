b5112e31c0156afa1187dcff814ea957a07e8c71
Alejandrito <alejandritoryzen11@Alejandritos-MacBook-Pro.local>
2024-12-01 22:14:01 -0500

    Chore: modify server execution to run in detached mode

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 # Correr el servidor
 echo "Iniciando el servidor..."
-docker run -it -v "$(pwd)/server/server_files:/app/server_files" -v "$(pwd)/server/db:/app/db" --name server1 --cap-add NET_ADMIN --network servers server
+docker run -d -v "$(pwd)/server/server_files:/app/server_files" -v "$(pwd)/server/db:/app/db" --name server1 --cap-add NET_ADMIN --network servers server
 
 # Correr el cliente
 echo "Iniciando el cliente..."
